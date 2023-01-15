3c21d4a220c758afc13e391e9fa9783b85686d00
AndreyGOIT <ae.borga@gmail.com>
2023-01-15 20:14:08 +0200

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,39 +1,39 @@
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  selectVisibleContacts,
+  selectContacts,
   selectIsLoading,
 } from 'redux/contacts/contacts-selectors';
 import styles from './ContactsList.module.css';
 import { deleteContact } from 'redux/contacts/contacts-operations';
 
 export const ContactsList = () => {
-  const contacts = useSelector(selectVisibleContacts);
+  const contacts = useSelector(selectContacts);
   const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
 
   console.log(contacts);
   return (
     <section className={styles.section}>
-      {contacts.length > 0 ? (
-        <ul>
-          {contacts.map(({ id, name, number }) => {
-            return (
-              <li key={id}>
-                {name}: {number}
-                <button
-                  onClick={() => {
-                    dispatch(deleteContact(id));
-                  }}
-                >
-                  {isLoading ? 'Deleting' : 'Delete'}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
+      {/* {contacts.length > 0 ? ( */}
+      <ul>
+        {contacts.map(({ id, name, number }) => {
+          return (
+            <li key={id}>
+              {name}: {number}
+              <button
+                onClick={() => {
+                  dispatch(deleteContact(id));
+                }}
+              >
+                {isLoading ? 'Deleting' : 'Delete'}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      {/* ) : (
         <p>There is no contacts yet</p>
-      )}
+      )} */}
     </section>
   );
 };
